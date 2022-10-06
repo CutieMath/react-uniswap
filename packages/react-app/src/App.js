d764896bd0ae8a4674b89e51033ee670de52781b
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-06 15:05:59 +1100

    set up front-page for uniswap

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { useEthers } from "@usedapp/core";
+import styles from "./styles";
+import { uniswapLogo } from "./assets";
 
 const App = () => {
-  return <p>Girl!</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img
+            src={uniswapLogo}
+            className="w-16 h-16 object-contain"
+            alt="logo"
+          />
+          Wallet button
+        </header>
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap x</h1>
+          <p className={styles.subTitle}>Exchange tokens in seconds</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
