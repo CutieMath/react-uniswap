b535d57a92136118455c19be6b6756ee852f2cb6
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-07 15:32:30 +1100

    web3 half setup

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,11 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0x449A78a83F045a48540bC3BF69E3363688253f14";
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]:
+      "https://eth-goerli.g.alchemy.com/v2/6ZYzlBdx3YQwMvb2EQVShIsnEe3_DrSu",
   },
 };
---
+++ packages/react-app/src/utils/getPairsInfo.js
@@ -0,0 +1,1 @@
+export const getPairInfo = async () => {};
